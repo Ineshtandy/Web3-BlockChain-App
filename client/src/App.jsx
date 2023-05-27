3b34d2a962e181cc0284c2ad70c4eca3c5e4b0a7
Inesh Tandon <i08polymath@gmail.com>
2023-05-27 23:51:17 +0530

    commit report -navbar completion in navbar.jsx -import ethers -added images folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
   );
-}
+};
 
 export default App;
